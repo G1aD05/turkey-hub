8f6b4e01885768465dc7dbee1879d8662324d05e
G1aD05 <ezrag2000@proton.me>
2026-08-17 21:02:36 -0700

    added loadstrings for new games

--- main.lua
+++ main.lua
@@ -8,8 +8,11 @@ elseif id == 133294838637122 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/G1aD05/turkey-hub/refs/heads/main/133294838637122.lua"))()
 elseif id == 97751951236815 then
     print("[Turkey Hub] Loading Guess My Logo script")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/G1aD05/turkey-hub/refs/heads/main/97751951236815.lua"))()
 elseif id == 100722736774753 then
     print("[Turkey Hub] Loading Guess My Football Country script")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/G1aD05/turkey-hub/refs/heads/main/100722736774753.lua"))()
 elseif id == 142823291 then
     print("[Turkey Hub] Loading Murder Mystery 2 script")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/G1aD05/turkey-hub/refs/heads/main/142823291.lua"))()
 end
